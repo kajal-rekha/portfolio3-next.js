2a4be04682903612fdc0ede8d660f4c4eb478e4b
kajal-2011 <kajalrekha888@gmail.com>
2023-06-28 10:18:36 +0600

    error fixed

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -31,106 +31,15 @@ const Contact = () => {
     e.target.querySelector(".message").value = "";
   };
 
-  <div className="contact wrapper mt-40">
-    <SectionTitle h2={"Contact"} />
-
-    <form
-      onSubmit={sendEmail}
-      className="mt-40 grid grid-cols-1 lg:grid-cols-2 gap-20"
-      ref={formRef}
-    >
-      <div className="form-control overflow-hidden">
-        <input
-          type="text"
-          placeholder="Write your name"
-          name="name"
-          required
-          className="fullname bg-transparent border outline-none  py-16 px-28 rounded-full border-cyan-400 duration-500 w-full"
-        />
-      </div>
-      <div className="form-control overflow-hidden">
-        <input
-          type="email"
-          placeholder="Write your email"
-          name="email"
-          required
-          className="email bg-transparent border py-16 px-28 outline-none rounded-full border-cyan-400 duration-500 w-full"
-        />
-      </div>
-      <div className="form-control overflow-hidden">
-        <textarea
-          placeholder="Write your message"
-          name="message"
-          required
-          rows="1"
-          cols="30"
-          className="message bg-transparent border py-16 px-28 rounded-full border-cyan-400  outline-none duration-500 w-full "
-        />
-      </div>
-
-      <div className="form-control overflow-hidden">
-        <input
-          type="submit"
-          value="Send message"
-          className="uppercase border bg-transparent py-16 px-28 rounded-full border-cyan-400 duration-500 w-full hover:bg-cyan-400/20 hover:border-cyan-400/20"
-        />
-      </div>
-    </form>
-  </div>;
   return (
     <div className="contact wrapper mt-40">
       <SectionTitle h2={"Contact"} />
-
-      {/* <form
-        onSubmit={sendEmail}
-        className="mt-32 grid grid-cols-1 lg:grid-cols-2 gap-20"
-      >
-        <div className="form-control overflow-hidden">
-          <input
-            type="text"
-            placeholder="Write your name"
-            name="fullname"
-            required
-            className="fullname bg-transparent border outline-none  py-12 px-28 rounded-lg border-white/70 focus:border-white  duration-500 w-full"
-          />
-        </div>
-        <div className="form-control overflow-hidden">
-          <input
-            type="email"
-            placeholder="Write your email"
-            name="email"
-            required
-            className="email bg-transparent border py-12 px-28 outline-none rounded-lg border-white/70 focus:border-white duration-500 w-full"
-          />
-        </div>
-        <div className="form-control overflow-hidden">
-          <textarea
-            placeholder="Write your message"
-            name="message"
-            required
-            rows="1"
-            cols="30"
-            className="message resize-none bg-transparent border py-12 px-28 rounded-lg border-white/70 focus:border-white outline-none duration-500 w-full "
-          />
-        </div>
-        <div className="form-control overflow-hidden">
-          <input
-            type="submit"
-            value="Send message"
-            className="uppercase border bg-transparent py-12 px-28 rounded-lg border-white/70 duration-500  focus:border-white w-full"
-          />
-        </div>
-      </form> */}
-
       <form
         onSubmit={sendEmail}
-        className="contact-form flex flex-col gap-5 py-10 mx-auto max-w-screen-sm"
+        className="contact-form grid grid-cols md:grid-cols-2 gap-5 py-10 mx-auto "
         ref={formRef}
       >
         <div className="form-control flex flex-col gap-2">
-          <label htmlFor="name" className="cursor-pointer">
-            Name
-          </label>
           <input
             type="name"
             name="name"
@@ -142,9 +51,6 @@ const Contact = () => {
           />
         </div>
         <div className="form-control flex flex-col gap-2">
-          <label htmlFor="email" className="cursor-pointer ">
-            Email Address
-          </label>
           <input
             type="email"
             name="email"
@@ -157,9 +63,6 @@ const Contact = () => {
         </div>
 
         <div className="form-control flex flex-col gap-2">
-          <label htmlFor="message" className="cursor-pointer">
-            Message
-          </label>
           <textarea
             placeholder="Write your message"
             required
